f4d552242383f97fb0e41489744c95c93ba69b88
Scott M. Likens <scott@likens.us>
2014-03-23 19:33:00 -0700

    let's remove verbosity

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ cd /opt/logstash/etc
  s/REDIS_PORT_6379_TCP_ADDR/${REDIS_PORT_6379_TCP_ADDR}/g;
  s/REDIS_PORT_6379_TCP_PORT/${REDIS_PORT_6379_TCP_PORT}/g" logstash.conf.in > logstash.conf
 cd /opt/logstash
-bin/logstash -v -f etc/logstash.conf
+bin/logstash -f etc/logstash.conf
